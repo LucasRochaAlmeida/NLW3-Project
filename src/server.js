208eb70f51d57096eb5f17157a71e527bb410a6f
Lucas <lucasrochadealmeida@hotmail.com>
2020-12-01 21:50:41 -0300

    backend, server, express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log("testando")
+//importar dependência
+const express = require('express');
+const path = require('path');
+
+//iniciando o express
+const server = express();
+server
+
+//utilizando arquivos estáticos
+.use(express.static('src/views'))
+
+//criar uma rota
+server.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+} )
+
+//ligar o servidor
+server.listen(5500)
