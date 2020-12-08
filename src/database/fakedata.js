7e7ea7fe79f8aed69bd3c30b3ddb39487032f809
Lucas <lucasrochadealmeida@hotmail.com>
2020-12-07 22:11:19 -0300

    começado a mexer nas imagens

---
+++ src/database/fakedata.js
@@ -0,0 +1,36 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        name: "Lar das meninas",
+        descripition: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1605999182551-96ab0c8ad8f9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+
+            "https://images.unsplash.com/photo-1598136490937-f77b0ce520fe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+
+
+    },
+    {
+        id: 2,
+        lat: "-27.222633",
+        lng: "-49.6555874",
+        name: "Lar das meninos",
+        descripition: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1605999182551-96ab0c8ad8f9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+
+            "https://images.unsplash.com/photo-1598136490937-f77b0ce520fe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+
+
+    }
+]
