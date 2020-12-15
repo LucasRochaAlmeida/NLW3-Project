f80d7e59a6ab08e22411fa2b9aaf77130787bde8
Lucas <lucasrochadealmeida@hotmail.com>
2020-12-14 21:09:09 -0300

    dados da tabela

--- src/database/test.js
+++ src/database/test.js
@@ -1,33 +1,28 @@
 const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage')
 
 Database.then(async db => {
     //Inserir dados na tabela
-    await db.run(`
-        INSERT INTO orphanages (
-            lat,
-            lng,
-            name,
-            about,
-            whatsapp,
-            images,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        ) VALUES (
-            "-27.222633",
-            "-49.6555874",
-            "Lar dos meninos",
-            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
-            "998989898",
+       await saveOrphanage(db, {
+        lat: "-27.222633",
+        lng: "-49.6655874",
+        name: "Lar de amor",
+        descripition: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        images: [
             "https://images.unsplash.com/photo-1605999182551-96ab0c8ad8f9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-            "Horário de visitas Das 18h até 8h",
-            "0"
-        );
-    `)
 
-    //consultar
+            "https://images.unsplash.com/photo-1598136490937-f77b0ce520fe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ].toString() ,
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+       })
+
+    //consultar dados da tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
 
     console.log(selectedOrphanages)
+
+    const orphanage  = await db.all('SELECT * FROM orphanages WHERE id = "3" ')
+    console.log(orphanage)
 } )
