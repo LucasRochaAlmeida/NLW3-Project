fa83ff33749e378329bb5ea548f77c0d908d1411
Lucas <lucasrochadealmeida@hotmail.com>
2020-12-14 21:08:13 -0300

    corrigindo erro de pt

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -20,7 +20,7 @@ module.exports = [
         id: 2,
         lat: "-27.222633",
         lng: "-49.6555874",
-        name: "Lar das meninos",
+        name: "Lar dos meninos",
         descripition: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
         images: [
             "https://images.unsplash.com/photo-1605999182551-96ab0c8ad8f9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
