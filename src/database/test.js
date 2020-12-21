51cec191e7f31ac8edc3bfb5d044ae02c067494d
Lucas <lucasrochadealmeida@hotmail.com>
2020-12-21 16:57:02 -0300

    testando banco de dados

--- src/database/test.js
+++ src/database/test.js
@@ -3,11 +3,12 @@ const saveOrphanage = require('./saveOrphanage')
 
 Database.then(async db => {
     //Inserir dados na tabela
-       await saveOrphanage(db, {
+        await saveOrphanage(db, {
         lat: "-27.222633",
-        lng: "-49.6655874",
-        name: "Lar de amor",
-        descripition: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        lng: "-49.6555874",
+        name: "Lar dos meninos",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        whatsapp: "998989898",
         images: [
             "https://images.unsplash.com/photo-1605999182551-96ab0c8ad8f9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
 
@@ -15,7 +16,7 @@ Database.then(async db => {
         ].toString() ,
         instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
         opening_hours: "Horário de visitas Das 18h até 8h",
-        open_on_weekends: "1"
+        open_on_weekends: "0"
        })
 
     //consultar dados da tabela
@@ -23,6 +24,10 @@ Database.then(async db => {
 
     console.log(selectedOrphanages)
 
-    const orphanage  = await db.all('SELECT * FROM orphanages WHERE id = "3" ')
+    //consultar somente 1 dado na tabela, pelo id
+    const orphanage  = await db.all('SELECT * FROM orphanages WHERE id = "2" ')
     console.log(orphanage)
+
+    //deletar dado da tabela
+    //console.log(await db.run("DELETE FROM orphanages WHERE id = '4' "))
 } )
