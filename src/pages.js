c709f1f9ecfaf50e613c431b40a71f1892130fe9
Lucas <lucasrochadealmeida@hotmail.com>
2021-01-04 21:46:38 -0300

    formulário

--- src/pages.js
+++ src/pages.js
@@ -1,3 +1,4 @@
+
 const Database = require('./database/db');
 const saveOrphanage = require('./database/saveOrphanage');
 
@@ -47,5 +48,39 @@ module.exports = {
     },
     createOrphanage(req, res){
         return res.render('create-orphanage')
+    },
+
+    async saveOrphanage(req, res){
+        const fields = req.body
+
+        //validar se todos os campos estão preechidos
+        if(Object.values(fields).includes('')){
+            return res.send('Todos os campos devem ser preenchidos!')
+        }
+
+        try{
+            //salvar um orfanato
+            const db = await Database
+            await saveOrphanage(db, {
+                lat: fields.lat,
+                lng: fields.lng,
+                name: fields.name,
+                about: fields.about,
+                whatsapp: fields.whatsapp,
+                images: fields.images.toString(),
+                instructions: fields.instructions,
+                opening_hours: fields.opening_hours,
+                open_on_weekends: fields.open_on_weekends,
+        })
+
+        //redirecionar
+        return res.redirect('/orphanages')
+
+        } catch(error){
+            console.log(error)
+            return res.send('Erro no banco de dados!')
+        }
+        
     }
+
 }
